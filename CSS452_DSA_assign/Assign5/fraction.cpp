8f555e9ffbd73a41e6fb0d27e47e13183263c56a
ThaHobbyist <dipdeys.dey@gmail.com>
2022-03-17 18:41:17 +0530

    Assign5

--- CSS452_DSA_assign/Assign5/fraction.cpp
+++ CSS452_DSA_assign/Assign5/fraction.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include<algorithm>
 using namespace std;
 
 class fraction {
@@ -7,10 +6,54 @@ private:
   int num, deno;
 
 public:
+  fraction operator+(const fraction &frac) {
+    fraction res;
+    res.num = num * frac.deno + deno * frac.num;
+    res.deno = deno * frac.deno;
+    res.deno = gcd(num, deno);
+    res.num /= gcd(num, deno);
+    return res;
+  }
+  fraction operator-(const fraction &frac) {
+    fraction res;
+    res.num = num * frac.deno - deno * frac.num;
+    res.deno = deno * frac.deno;
+    res.deno = gcd(num, deno);
+    res.num /= gcd(num, deno);
+    return res;
+  }
+  fraction operator*(const fraction &frac) {
+    fraction res;
+    res.num = num * frac.num;
+    res.deno = deno * frac.deno;
+    res.deno = gcd(num, deno);
+    res.num /= gcd(num, deno);
+    return res;
+  }
+  fraction operator/(const fraction &frac) {
+    fraction res;
+    res.num = num * frac.deno;
+    res.deno = deno * frac.num;
+    res.deno = gcd(num, deno);
+    res.num /= gcd(num, deno);
+    return res;
+  }
+  fraction operator*() {
+    fraction res;
+    res.deno = gcd(num, deno);
+    res.num /= gcd(num, deno);
+    return res;
+  }
+  bool operator==(const fraction &frac) {
+    if(num == )
+  }
+  
+  
+  
   
 };
 
 int main() {
-  cout<<__gcd(3, 6);
+  cout<<gcd(3, 6);
   return 0;
 }
