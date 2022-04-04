21cac883f5d14bfa4256fa95ca491cc80623c23a
ThaHobbyist <dipdeys.dey@gmail.com>
2022-04-04 15:08:15 +0530

    added computer org assignment

---
+++ CSS451/calculator.asm
@@ -0,0 +1,34 @@
+.data 
+	m1: .asciiz
+	m2: .asciiz
+	m3: .asciiz
+	m4: .asciiz
+	
+.text
+main: 
+	#Menu
+	li $v0, 4
+	la $a0, ml
+	syscall
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	# Input 1
+	li $v0, 4
+	la $a0, m2
+	syscall
+	li $v0, 5
+	syscall
+	move $t2,$v0
+	
+	#Input 2
+	li $v0,4
+	la $a0,m3
+	syscall
+	li $v0,5
+	syscall
+	move $t3,$v0
+	
+	#Switch
+	beg $t0,1,case 1
